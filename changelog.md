2e4cfb7f53a2888bb9455cd16f1b0c6633c9c704
Joost Melet <joostmsoftware@gmail.com>
2023-09-09 12:26:53 +0200

    see changelog.md + version bump

---
+++ changelog.md
@@ -0,0 +1,9 @@
+# Changelog 
+
+## version 0.2
+
+- Added a config module.
+- Added a TagRegistar class for registering tags a bit easier. 
+- Updated the Registar class.
+- Renamed ArmorConstructor to ArmorBuilder.
+- Added DamageableToolCraftingItem & adjusted a small thing in DamageableCraftingItem.
---
+++ src/main/java/com/kirbosoftware/poyolib/api/v1/core/registry/TagRegistar.java
@@ -0,0 +1,40 @@
+package com.kirbosoftware.poyolib.api.v1.core.registry;
+
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TagRegistar<T> {
+    private final RegistryKey<? extends Registry<T>> key;
+    private final String modId;
+
+    private final List<String> entries = new ArrayList<>();
+    public TagRegistar(RegistryKey<? extends Registry<T>> key, String modId) {
+        this.key = key;
+        this.modId = modId;
+    }
+
+    public TagKey<T> register(String path) {
+        if (entries.contains(path)) {
+            throw new IllegalArgumentException("Duplicate registration " + path);
+        }
+
+        return TagKey.of(getKey(), new Identifier(getModId(), path));
+    }
+
+    public RegistryKey<? extends Registry<T>> getKey() {
+        return key;
+    }
+
+    public String getModId() {
+        return modId;
+    }
+
+    public List<String> getEntries() {
+        return entries;
+    }
+}
