4cf7dc218b925ce82d3912ca218fd15017851845
Joost <joostmsoftware@gmail.com>
2023-10-15 20:42:08 +0200

    v0.2.1.2

--- changelog.md
+++ changelog.md
@@ -1,8 +1,12 @@
 # Changelog 
 
+## v0.2.1.2
+- renamed `Food` to `FoodType`
+- created a `Food` class for people to extend to instead of FoodType
+
 ## v0.2.1 && v0.2.1.1
 ### v0.2.1.1 only:
-- fixed a crash relating to the rework of food stuff.
+- fixed a crash relating to the rework of foodstuff.
 ### v0.2.1
 - updated the way how you make foodstuff. Now you have an abstract class you would need to extend to and create your own food class to register.
 
---
+++ src/main/java/com/kirbosoftware/poyolib/api/v1/item/food/Food.java
@@ -0,0 +1,26 @@
+package com.kirbosoftware.poyolib.api.v1.item.food;
+
+public class Food extends FoodType{
+    private int hunger = 0;
+    private float fullness = 0.0f;
+
+    public Food setHunger(int hunger) {
+        this.hunger = hunger;
+        return this;
+    }
+
+    public Food setFullness(float fullnessModifier) {
+        fullness = fullnessModifier;
+        return this;
+    }
+
+    @Override
+    public int setHunger() {
+        return hunger;
+    }
+
+    @Override
+    public float setFullness() {
+        return fullness;
+    }
+}
